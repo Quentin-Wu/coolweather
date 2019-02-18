409fb0293c4f61bb2163d645bf9cdca4af534587
wu.guoting <wu.guoting@foxmail.com>
2019-02-18 22:31:20 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/wuguoting/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.example.wuguoting.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wu.guoting on 2019/2/18.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
